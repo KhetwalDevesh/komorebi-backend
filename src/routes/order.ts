e6a8e5008340c23b76711d6b10bbd960cb5ad654
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-04-07 12:12:50 +0530

    init and final

---
+++ src/routes/order.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { createOrder } from "../controllers/order";
+
+const orderRoutes = express.Router();
+orderRoutes.post("/", createOrder);
+
+export default orderRoutes;
